d58b97c257e945b8d9f26e400e6d2a11edf19798
Roberto dos Santos de Lima <roslima@mag.com.br>
2026-06-30 09:26:14 -0300

    feat: adiciona suporte a PWA (instalacao como app no Android/iOS)

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'adfare-dizimos-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/logo_adfare.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
